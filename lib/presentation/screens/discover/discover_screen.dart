254e7413edb792bc503ea638d3b70164764707de
Santiago Millan <santiagomillanpardo@gmail.com>
2024-03-12 23:12:13 -0500

    CircularProgress y pageView

--- lib/presentation/screens/discover/discover_screen.dart
+++ lib/presentation/screens/discover/discover_screen.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:toktik/presentation/providers/discover_provider.dart';
+import 'package:toktik/presentation/widgets/shared/video_scrollable_view.dart';
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('DiscoverScreen'),
-      ),
+    final discoverProvider = context.watch<DiscoverProvider>();
+    // final otroProvider = Provider.of<DiscoverProvider>(context);
+    return Scaffold(
+      body: discoverProvider.initialLoading
+          ? const Center(
+              child: CircularProgressIndicator(
+              strokeWidth: 2,
+            ))
+          : VideoScrollableView(videos: discoverProvider.videos),
     );
   }
 }
