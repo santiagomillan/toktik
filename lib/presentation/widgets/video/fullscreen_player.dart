48b72a263d4d6bbeefbf3442eb2d95b3924bf33f
Santiago Millan <santiagomillanpardo@gmail.com>
2024-03-14 00:27:42 -0500

    Reproductor de video p1

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class FullScreenPlayer extends StatelessWidget {
+class FullScreenPlayer extends StatefulWidget {
   final String videoUrl;
   final String caption;
 
@@ -8,7 +9,36 @@ class FullScreenPlayer extends StatelessWidget {
       {super.key, required this.videoUrl, required this.caption});
 
   @override
+  State<FullScreenPlayer> createState() => _FullScreenPlayerState();
+}
+
+class _FullScreenPlayerState extends State<FullScreenPlayer> {
+  late VideoPlayerController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = VideoPlayerController.asset(widget.videoUrl)
+      ..setVolume(0)
+      ..setLooping(true)
+      ..play();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  // bool isPlaying =  true;
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return FutureBuilder(
+      future: controller.initialize(),
+      builder: (context, snapshot) {
+        return const Center(
+          child: CircularProgressIndicator(strokeWidth: 2),
+        );
+      },
+    );
   }
 }
