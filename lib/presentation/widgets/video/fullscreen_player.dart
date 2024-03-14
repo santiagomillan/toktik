02fea185ab274cc5c2806996feca8c7f41072f4d
Santiago Millan <santiagomillanpardo@gmail.com>
2024-03-14 01:01:21 -0500

    Pause Video

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -39,21 +39,30 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
             child: CircularProgressIndicator(strokeWidth: 2),
           );
         }
-        return AspectRatio(
-          aspectRatio: controller.value.aspectRatio,
-          child: Stack(
-            children: [
-              VideoPlayer(controller),
-              //gradiente
+        return GestureDetector(
+          onTap: () {
+            if (controller.value.isPlaying) {
+              controller.pause();
+              return;
+            }
+            controller.play();
+          },
+          child: AspectRatio(
+            aspectRatio: controller.value.aspectRatio,
+            child: Stack(
+              children: [
+                VideoPlayer(controller),
+                //gradiente
 
-              //texto
-              Positioned(
-                  bottom: 50,
-                  left: 20,
-                  child: _VideoCaption(
-                    caption: widget.caption,
-                  ))
-            ],
+                //texto
+                Positioned(
+                    bottom: 50,
+                    left: 20,
+                    child: _VideoCaption(
+                      caption: widget.caption,
+                    ))
+              ],
+            ),
           ),
         );
       },
