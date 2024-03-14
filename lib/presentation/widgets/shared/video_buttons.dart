2e86a9550ee9f90712de776a40e87cc708a8092f
Santiago Millan <santiagomillanpardo@gmail.com>
2024-03-13 23:11:02 -0500

    animate_do

--- lib/presentation/widgets/shared/video_buttons.dart
+++ lib/presentation/widgets/shared/video_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:toktik/config/helpers/human_formats.dart';
 import 'package:toktik/domain/video_post.dart';
@@ -16,9 +17,19 @@ class VideoButtons extends StatelessWidget {
           iconColor: Colors.red,
           iconData: Icons.favorite,
         ),
+        const SizedBox(height: 20),
         _CustomIconButton(
           value: video.views,
           iconData: Icons.remove_red_eye_outlined,
+        ),
+        const SizedBox(height: 20),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration(seconds: 5),
+          child: const _CustomIconButton(
+            value: 0,
+            iconData: Icons.play_circle_outlined,
+          ),
         )
       ],
     );
@@ -44,7 +55,7 @@ class _CustomIconButton extends StatelessWidget {
               color: color,
               size: 30,
             )),
-        Text(HumanFormats.humanReadbleNumber(value.toDouble()))
+        if (value > 0) Text(HumanFormats.humanReadbleNumber(value.toDouble()))
       ],
     );
   }
