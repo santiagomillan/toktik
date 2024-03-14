10ec6aff9f953ab691fd89e0fa5779bdd0af04fd
Santiago Millan <santiagomillanpardo@gmail.com>
2024-03-14 18:51:06 -0500

    Usando el repositorio y origen de datos.

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
-import 'package:toktik/infraestucture/models/local_video_model.dart';
-import 'package:toktik/shared/data/local_video_post.dart';
+import 'package:toktik/domain/repositories/video_posts_repository.dart';
 
 class DiscoverProvider extends ChangeNotifier {
-  //TODO Repository, DataSource
+  final VideoPostRepository videosRepository;
 
   bool initialLoading = true;
   List<VideoPost> videos = [];
+
+  DiscoverProvider({required this.videosRepository});
 
   Future<void> loadNextPage() async {
     //metodo espera 2 seg
     // await Future.delayed(const Duration(seconds: 2));
     //Carga los videos
-    final List<VideoPost> newVideos = videoPosts
-        .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
-        .toList();
+    // final List<VideoPost> newVideos = videoPosts
+    //     .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
+    //     .toList();
+    final newVideos = await videosRepository.getTrendingVideosByPage(1);
 
     //a;ade los videos
     videos.addAll(newVideos);
